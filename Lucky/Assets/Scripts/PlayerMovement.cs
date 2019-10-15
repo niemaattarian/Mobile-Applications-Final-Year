cd47514c353ef07ac9cdf84825d14abdc835801d
niemaattarian <G00346901@gmit.ie>
2019-10-16 00:50:24 +0100

    Began game functionality, included in-game movement and in-game physics.

---
+++ Lucky/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+       
+    }
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+
+        //Animation
+
+        //Player Direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //Jumping
+    }
+
+    void FlipPlayer()
+    {
+
+    }
+
+}
