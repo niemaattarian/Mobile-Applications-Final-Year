6fb6aecd084681f985de03615febd3f19e1e84ba
niemaattarian <G00346901@gmit.ie>
2019-11-03 16:36:27 +0000

    Added health bar which decreases when struck by enemy

---
+++ Lucky/Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    Image health;
+    float maxHealth = 100f;
+    public static float playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<Image> ();
+        playerHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.fillAmount = playerHealth / maxHealth;
+    }
+}
