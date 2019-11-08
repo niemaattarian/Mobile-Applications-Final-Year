2292a6c6fefd3763dbaf458f33bb8bf80a0db67f
niemaattarian <G00346901@gmit.ie>
2019-11-08 18:00:38 +0000

    Fixed camera to follow player and smoothed it out. Added more sprites to the game

---
+++ Lucky/Assets/Scripts/GameScripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Gameobject PauseMenu, PauseButton;
+
+    // This stops game time when the game is paused and in the pause menu
+    public void Pause(){
+        Time.timeScale = 0;
+    }
+
+    // This Resumes the game once the resume button is pressed
+    public void Resume(){
+        Time.timeScale = 1;
+    }
+}
---
+++ Lucky/Assets/Scripts/GameScripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = true;
+    public int playerJumpPower = 1250;
+    private float moveX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+       
+    }
+
+    
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown ("Jump"))
+        {
+            Jump();
+        }
+        //Animation
+
+        //Player Direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //Jumping
+        GetComponent<Rigidbody2D>().AddForce (Vector2.up * playerJumpPower);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("EnergyPickup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    
+    void FlipPlayer()
+    {
+
+    }
+
+}
