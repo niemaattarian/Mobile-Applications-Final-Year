4c9f915b9de4b528ab64a9fd58071b532281fe24
niemaattarian <G00346901@gmit.ie>
2019-11-10 19:46:42 +0000

    Fixed Opening menu, user can now press any key to start

---
+++ Lucky/Assets/Scripts/MenuScripts/OpenScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenScene : MonoBehaviour
+{
+    public AudioClip sfxButton;
+
+    private bool oneshotSfx;
+
+    // Update once per frame
+    void Update()
+    {
+        // If any key is pressed, jump to gameplay scene
+        if(Input.anyKeyDown)
+        {
+            if(!oneshotSfx)
+            {
+                //AudioSource.PlayClipAtPoint(sfxButton, Vector3.zero);
+                Invoke("LoadScene", 0.5f);
+                oneshotSfx = true;
+            }
+        }
+    }
+
+    void LoadScene()
+    {
+        //Load gameplay scene
+        Application.LoadLevel("Scenes/MenuScenes/MainMenu");
+    }
+}
